6fcc5f20348f9f9c6005fc1e5552a0cdcb828978
KH <knhg2013@gmail.com>
2020-04-11 12:52:38 -0400

    Add the User Model and apply the Express Validator

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+const { check, validationResult } = require('express-validator/check');
+//The require('express-validator/check') also works
+
+
+const User = require('../models/Users');
+
 // @route       POST api/users
 // @desc        Register a user
 // @access      Public     
-router.post('/', (req, res) => {
-    res.send('Register a user');
+router.post('/',[
+        check('username', 'Please enter UserName').notEmpty(),
+        // the 'notEmpty'need replace to: .not().isEmpty() ???
+        check('email','Please enter a valid email').isEmail(),
+        check('password', 'Please enter at leat 5 characters')
+            .isLength({min:5})
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){ 
+            // Above, don't know why '.notEmpty()' doesn't work
+            return res.status(400).json({errors: errors.array()});
+        }  else {
+            res.send("passed");
+        }
 });
 
 module.exports = router;
