238117438150909eca883b9c15f01da6b8ead015
KH <knhg2013@gmail.com>
2020-04-13 17:30:12 -0400

    update Contact Model and Get Contacts Route (sec 47)

--- middleWare/auth.js
+++ middleWare/auth.js
@@ -16,9 +16,6 @@ module.exports = function(req, res, next) {
         next();        
     } catch (err) {
         res.status(401).json({msg: 'Token error, access denied'});
-    }
-
-    
-    
+    } 
 
 }
--- routes/contacts.js
+++ routes/contacts.js
@@ -2,12 +2,26 @@
 
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator/check');
+const auth = require('../middleWare/auth');
+
+const Contact = require('../models/Contacts');
+const User = require('../models/Users');
 
 // @route       GET api/contacts
 // @desc        Get all the user contacts
 // @access      Private     
-router.get('/', (req, res) => {
-    res.send('Get all contacts');
+router.get('/', auth, async (req, res) => {
+    try {
+        const contacts = await Contact.find({user: req.user.id}).sort({date: -1});
+        // above line, don' know whynot is:  '{id: req.user.id}'
+        res.json(contacts);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({msg: 'Server Error'});
+    }
+
+    //res.send('Get all contacts');
 });
 
 
